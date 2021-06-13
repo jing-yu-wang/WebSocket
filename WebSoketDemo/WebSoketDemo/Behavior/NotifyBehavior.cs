883bb7ecc60ac2a87b800939aeb2520e705d58ae
jing-yu-wang <85814153+jing-yu-wang@users.noreply.github.com>
2021-06-13 10:54:48 +0800

    Add files via upload

---
+++ WebSoketDemo/WebSoketDemo/Behavior/NotifyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSoketDemo.Behavior
+{
+    public class NotifyBehavior : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Send(JsonConvert.SerializeObject(new { message = "Open" }));
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            string msg = e.Data;
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebSoketDemo/WebSoketDemo/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Http;
+using WebSocketSharp.Server;
+using WebSoketDemo.Behavior;
+
+namespace WebSoketDemo
+{
+    public class WebApiApplication : HttpApplication
+    {
+        public static WebSocketServer WebSocketServer { get; private set; }
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            WebSocketServer = new WebSocketServer(5000);
+            WebSocketServer.AddWebSocketService<NotifyBehavior>("/notify");
+            WebSocketServer.Start();
+        }
+    }
+}
